d72dca85a253dbef1ec9cbbc15d713dc59324590
Mélanie <melanie.c@digitalcube.net>
2023-12-04 14:34:40 +0100

    add s

--- src/api/categorie/routes/custom.js
+++ src/api/categorie/routes/custom.js
@@ -2,7 +2,7 @@ module.exports = {
     routes: [
         {
             method: 'GET',
-            path: '/categorie/:slug',
+            path: '/categories/:slug',
             handler: 'categorie.findOne',
             config: {
                 auth: false,
