8b4b4becc1d0de075d1291b4a914137167d349ed
Mélanie Croce <melaniecroce@MacBook-Pro-de-Melanie.local>
2024-01-04 14:52:12 +0100

    fix populate

--- src/api/categorie/controllers/categorie.js
+++ src/api/categorie/controllers/categorie.js
@@ -9,9 +9,11 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::categorie.categorie', ({strapi}) => ({
     async findOne(ctx) {
         const {slug} = ctx.params;
-        const entity =  await strapi.db.query('api::categorie.categorie').findOne({
-            where: {slug},
-            populate:  true,
+        const entity = await strapi.entityService.findMany('api::categorie.categorie', {
+            filters:{
+                slug: slug
+            },
+            populate : "*"
         });
         const sanitizedEntity = await this.sanitizeOutput(entity);
 
